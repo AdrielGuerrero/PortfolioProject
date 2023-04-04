57946701037361467df999b7e6a59b929f71b743
AdrielGuerrero <129806379+AdrielGuerrero@users.noreply.github.com>
2023-04-04 17:42:59 -0500

    Update SQLQuery PROJECT.sql

--- SQLQuery PROJECT.sql
+++ SQLQuery PROJECT.sql
@@ -1,22 +1,72 @@
 
+--COVID 19 DATA EXPLORATION
+
+
+-- Skills Used: Joins, CTE's, Windows Functions, Aggregate Functions, Creating Views, Converting Data Tyopes
 
 
 
---select * from PortfolioProject..CovidVaccinations
---order by 3,4
+select * from PortfolioProject..CovidVaccinations
+where continent is not null 
+order by 3,4
 
---This is to show how many people died per the cases
-select location,date,total_cases, total_deaths, (total_deaths/total_cases)*100 as Death_by_Cases
+select * from PortfolioProject..CovidDeaths
+where continent is not null 
+order by 3,4
+
+
+
+
+
+
+-- Select Data that we are going to be starting with
+
+Select Location, date, total_cases, new_cases, total_deaths, population
+From PortfolioProject..CovidDeaths
+Where continent is not null 
+order by 1,2
+
+
+
+
+
+
+
+
+-- TOTAL CASES vs TOTAL DEATHS
+-- Shows likelihood of dying if you contract covid in your country
+
+
+select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as Death_by_Cases
 from PortfolioProject..CovidDeaths 
 where location like '%states%' and continent is not null
 order by 1,2
 
 
---This is to show how many people got cases compared to population
+
+
+
+
+
+
+
+
+-- Total Cases vs Population
+-- Shows what percentage of population infected with Covid
+
 select location,date,population, total_cases, (total_cases/population)*100 as 'Percent Populaiton Infected'
 from PortfolioProject..CovidDeaths 
 where location like '%states%' and continent is not null
 order by 1,2
+
+
+
+
+
+
+
+
+
 
 
 --Countries with highest infection rate compared to population
@@ -27,23 +77,38 @@ where continent is not null
 group by population,location
 order by 'Percent Population Infected' desc
 
+
+
+
+
+
+
+
+
 --How many people died from COVID per population
 
 select location, Max(cast(total_deaths as int)) as TotalDeathCount 
 from PortfolioProject..CovidDeaths 
 --where location like '%states%'
-
 where continent is not null
-
-
 group by location
-
 order by TotalDeathCount desc
 
 
 
 
--- By Continent with highest death count
+
+
+
+
+
+
+
+-- BREAKING THINGS DOWN BY CONTINENT
+
+-- Showing contintents with the highest death count per population
+
+
 select continent, Max(cast(total_deaths as int)) as TotalDeathCount 
 from PortfolioProject..CovidDeaths 
 --where location like '%states%'
@@ -53,48 +118,71 @@ order by TotalDeathCount desc
 
 
 
+
+
+
+
+
 -- Global Numbers
 
-select sum(new_cases) as total_cases, sum (cast(new_deaths as int)) as total_deaths, sum(cast(new_deaths as int)) / sum(new_cases)*100 as DeathPercentage --, total_deaths (total_deaths/total_cases)*100 as 'Percent Populaiton Infected'
-from PortfolioProject..CovidDeaths 
---where location like '%states%' 
-where continent is not null
---group by date
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+where continent is not null 
+--Group By date
 order by 1,2
 
 
--- Looking at total population vs vaccinations
-select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
--- agregate function
-Sum(Cast(vac.new_vaccinations as int)) OVER (Partition by dea.location order by dea.location, dea.date) as RollingPeopleVaccinated
-, (RollingPeopleVaccinated/population) * 100
-from PortfolioProject..CovidDeaths dea
+
+
+
+
+
+
+
+-- Total Population vs Vaccinations
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
+
+
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From PortfolioProject..CovidDeaths dea
 Join PortfolioProject..CovidVaccinations vac
- on dea.location = vac.location
- and dea.date = vac.date
- where dea.continent is not null
- order by 2,3
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+order by 2,3
 
 
- -- USE CTE
 
- with PopvsVac (continent, location, date, population, new_vaccinations, RollingPeopleVaccinated)
- as
- (
- select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
--- agregate function
-Sum(CONVERT(bigint,vac.new_vaccinations)) OVER (Partition by dea.location order by dea.location, dea.date) as RollingPeopleVaccinated
---, (RollingPeopleVaccinated/population) * 100
-from PortfolioProject..CovidDeaths dea
+
+
+
+
+
+-- Using CTE to perform Calculation on Partition By in previous query
+
+
+With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
+as
+(
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From PortfolioProject..CovidDeaths dea
 Join PortfolioProject..CovidVaccinations vac
- on dea.location = vac.location
- and dea.date = vac.date
- where dea.continent is not null
--- order by 2,3
- )
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+--order by 2,3
+)
+Select *, (RollingPeopleVaccinated/Population)*100
+From PopvsVac
 
-select * , (RollingPeopleVaccinated/Population) * 100
-from PopvsVac
+
+
+
 
 
 
@@ -104,7 +192,6 @@ from PopvsVac
 
 Create View PercentPopulationVaccinated as
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
--- agregate function
 Sum(CONVERT(bigint,vac.new_vaccinations)) OVER (Partition by dea.location order by dea.location, dea.date) as RollingPeopleVaccinated
 --, (RollingPeopleVaccinated/population) * 100
 from PortfolioProject..CovidDeaths dea
